f47c3a76b526d8c4ec080b3f965c9f25df4a535f
kPlusPlus <kresimir.ivkovic@gmail.com>
2024-03-15 10:14:08 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace ExplorerKA
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            txtFileName = new TextBox();
+            trvDirs = new TreeView();
+            lstViewDirsFiles = new ListView();
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            tsmOpen = new ToolStripMenuItem();
+            tsmCompress = new ToolStripMenuItem();
+            panel1 = new Panel();
+            tsmRefresh = new ToolStripMenuItem();
+            contextMenuStrip1.SuspendLayout();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // txtFileName
+            // 
+            txtFileName.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            txtFileName.Location = new Point(3, 3);
+            txtFileName.Name = "txtFileName";
+            txtFileName.Size = new Size(1263, 23);
+            txtFileName.TabIndex = 0;
+            // 
+            // trvDirs
+            // 
+            trvDirs.AllowDrop = true;
+            trvDirs.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            trvDirs.BackColor = Color.MidnightBlue;
+            trvDirs.ForeColor = Color.Yellow;
+            trvDirs.Location = new Point(3, 32);
+            trvDirs.Name = "trvDirs";
+            trvDirs.Size = new Size(381, 807);
+            trvDirs.TabIndex = 1;
+            trvDirs.AfterSelect += trvDirs_AfterSelect;
+            // 
+            // lstViewDirsFiles
+            // 
+            lstViewDirsFiles.AllowColumnReorder = true;
+            lstViewDirsFiles.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            lstViewDirsFiles.BackColor = Color.MidnightBlue;
+            lstViewDirsFiles.ContextMenuStrip = contextMenuStrip1;
+            lstViewDirsFiles.ForeColor = Color.Yellow;
+            lstViewDirsFiles.Location = new Point(390, 32);
+            lstViewDirsFiles.Name = "lstViewDirsFiles";
+            lstViewDirsFiles.Size = new Size(876, 807);
+            lstViewDirsFiles.TabIndex = 2;
+            lstViewDirsFiles.UseCompatibleStateImageBehavior = false;
+            lstViewDirsFiles.View = View.List;
+            lstViewDirsFiles.DoubleClick += lstViewDirsFiles_DoubleClick;
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.Items.AddRange(new ToolStripItem[] { tsmOpen, tsmCompress, tsmRefresh });
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(181, 92);
+            // 
+            // tsmOpen
+            // 
+            tsmOpen.Name = "tsmOpen";
+            tsmOpen.Size = new Size(180, 22);
+            tsmOpen.Text = "Open";
+            tsmOpen.Click += tsmOpen_Click;
+            // 
+            // tsmCompress
+            // 
+            tsmCompress.Name = "tsmCompress";
+            tsmCompress.Size = new Size(180, 22);
+            tsmCompress.Text = "Compress";
+            tsmCompress.Click += tsmCompress_Click;
+            // 
+            // panel1
+            // 
+            panel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            panel1.BackColor = Color.Brown;
+            panel1.Controls.Add(lstViewDirsFiles);
+            panel1.Controls.Add(txtFileName);
+            panel1.Controls.Add(trvDirs);
+            panel1.Location = new Point(12, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1269, 842);
+            panel1.TabIndex = 3;
+            // 
+            // tsmRefresh
+            // 
+            tsmRefresh.Name = "tsmRefresh";
+            tsmRefresh.Size = new Size(180, 22);
+            tsmRefresh.Text = "Refresh";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1293, 866);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Form1";
+            contextMenuStrip1.ResumeLayout(false);
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        public TextBox txtFileName;
+        private TreeView trvDirs;
+        private ListView lstViewDirsFiles;
+        private Panel panel1;
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem tsmOpen;
+        private ToolStripMenuItem tsmCompress;
+        private ToolStripMenuItem tsmRefresh;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,325 @@
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+//using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+using ICSharpCode.SharpZipLib.Zip;
+//using SevenZip;
+//using SevenZip.Sdk.Compression.Lzma;
+using CompressionLevel = System.IO.Compression.CompressionLevel;
+
+
+
+namespace ExplorerKA
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("shell32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr SHGetFileInfo(string pszPath, uint dwFileAttributes, ref SHFILEINFO psfi, uint cbFileInfo, uint uFlags);
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        public struct SHFILEINFO
+        {
+            public IntPtr hIcon;
+            public int iIcon;
+            public uint dwAttributes;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
+            public string szDisplayName;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 80)]
+            public string szTypeName;
+        }
+
+        private const uint SHGFI_ICON = 0x100;
+        private const uint SHGFI_SMALLICON = 0x1;
+        private const uint SHGFI_LARGEICON = 0x0;
+
+        private ImageList imageList; // Icon of file
+        private TreeNode selectedNode; // tree of directory
+        private DirectoryInfo directoryInfo;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeImageList();
+            InitializeListView();
+            PopulateTreeView();
+        }
+
+        private void InitializeImageList()
+        {
+            imageList = new ImageList();
+            imageList.ImageSize = new Size(32, 32); // Set the desired icon size
+            imageList.ColorDepth = ColorDepth.Depth32Bit;
+            lstViewDirsFiles.SmallImageList = imageList;
+        }
+
+        private void InitializeListView()
+        {
+            lstViewDirsFiles.View = View.Details;
+            lstViewDirsFiles.Columns.Add("Name", 150); // Column for file/folder namewq
+            lstViewDirsFiles.Columns.Add("Size", 100); // Column for file size
+            lstViewDirsFiles.Columns.Add("Last Modified", 150); // Column for last modified timestamp
+
+            //lstViewDirsFiles.ContextMenuStrip = contextMenuStrip1;
+        }
+
+
+
+        private void PopulateTreeView()
+        {
+            foreach (var drive in DriveInfo.GetDrives())
+            {
+                TreeNode driveNode = new TreeNode(drive.Name);
+                driveNode.Tag = drive.RootDirectory;
+                trvDirs.Nodes.Add(driveNode);
+
+                //PopulateDirectories(drive.RootDirectory, driveNode);
+                PopulateFilesAndDirectories(drive.RootDirectory);
+            }
+        }
+
+        private void PopulateDirectories(DirectoryInfo directory, TreeNode parentNode)
+        {
+            try
+            {
+                foreach (var subDirectory in directory.GetDirectories())
+                {
+                    TreeNode node = new TreeNode(subDirectory.Name);
+                    node.Tag = subDirectory;
+                    parentNode.Nodes.Add(node);
+                    PopulateDirectories(subDirectory, node);
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                // Handle unauthorized access (e.g., access denied to a folder)
+            }
+        }
+
+        private void PopulateFiles(DirectoryInfo directory)
+        {
+            lstViewDirsFiles.Items.Clear();
+
+            try
+            {
+                foreach (var file in directory.GetFiles())
+                {
+                    var item = new ListViewItem(file.Name);
+                    item.SubItems.Add(file.Length.ToString());
+                    item.SubItems.Add(file.LastWriteTime.ToString());
+                    lstViewDirsFiles.Items.Add(item);
+                }
+            }
+            catch (UnauthorizedAccessException uae)
+            {
+                // Handle unauthorized access (e.g., access denied to a file)
+                Debug.WriteLine("Error 21. - unauthorized access " + uae.ToString());
+            }
+            catch (System.IO.IOException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+        private void PopulateFilesAndDirectories(DirectoryInfo directory)
+        {
+            lstViewDirsFiles.Items.Clear();
+            int ctlCounter = 0;
+
+            try
+            {
+                if (trvDirs.SelectedNode != null) PopulateDirectories(directory, trvDirs.SelectedNode); //TODO: TEST TEST TEST
+
+                foreach (var subDirectory in directory.GetDirectories())
+                {
+                    var item = new ListViewItem(subDirectory.Name);
+                    item.SubItems.Add("Folder"); // Indicate that it's a folder
+                    item.SubItems.Add(subDirectory.LastWriteTime.ToString()); // Last write time
+                    item.Tag = subDirectory; // Store the DirectoryInfo object for later use
+                    var icon = GetFileIcon(subDirectory.FullName);// Get the icon for the file
+                    imageList.Images.Add(icon);
+                    item.ImageIndex = imageList.Images.Count - 1;
+                    lstViewDirsFiles.Items.Add(item);
+                    ctlCounter++;
+                }
+
+                foreach (var file in directory.GetFiles())
+                {
+                    var item = new ListViewItem(file.Name);
+                    item.SubItems.Add(file.Length.ToString()); // File size
+                    item.SubItems.Add(file.LastWriteTime.ToString()); // Last write time
+                    item.Tag = file; // Store the FileInfo object for later use
+                    var icon = GetFileIcon(file.FullName);// Get the icon for the file
+                    imageList.Images.Add(icon);
+                    item.ImageIndex = imageList.Images.Count - 1;
+                    lstViewDirsFiles.Items.Add(item);
+                }
+            }
+            catch (UnauthorizedAccessException uex)
+            {
+                // Handle unauthorized access (e.g., access denied to a file or folder)
+                Debug.WriteLine(uex.Message);
+            }
+            catch (System.IO.IOException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+        private void trvDirs_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            selectedNode = e.Node;
+            if (selectedNode != null && selectedNode.Tag is DirectoryInfo directory)
+            {
+                txtFileName.Text = directory.FullName;
+                //PopulateFiles(directory);
+                //PopulateFilesAndDirectories(directory);
+                PopulateFilesAndDirectories(directory); //TODO: TEST TEST TEST
+                trvDirs.SelectedNode = selectedNode;
+                directoryInfo = new DirectoryInfo(directory.FullName); // to variable
+            }
+        }
+
+        private Icon GetFileIcon(string filePath)
+        {
+            SHFILEINFO shinfo = new SHFILEINFO();
+            IntPtr hImg = SHGetFileInfo(filePath, 0, ref shinfo, (uint)Marshal.SizeOf(shinfo), SHGFI_ICON | SHGFI_SMALLICON);
+            if (hImg == IntPtr.Zero)
+            {
+                throw new FileNotFoundException("Icon not found");
+            }
+            Icon icon = Icon.FromHandle(shinfo.hIcon);
+            return icon;
+        }
+
+
+        private void lstViewDirsFiles_DoubleClick(object sender, EventArgs e)
+        {
+            if (lstViewDirsFiles.SelectedItems.Count > 0)
+            {
+                var selectedItem = lstViewDirsFiles.SelectedItems[0];
+                DirectoryInfo di = selectedItem.Tag as DirectoryInfo;
+
+                if (di != null)
+                {
+                    PopulateFilesAndDirectories(di);
+                    PopulateDirectories(di, selectedNode);
+                    txtFileName.Text = di.FullName;
+                    directoryInfo = di;
+                }
+            }
+
+        }
+
+        private void tsmOpen_Click(object sender, EventArgs e)
+        {
+            if (lstViewDirsFiles.SelectedItems.Count > 0)
+            {
+                ListViewItem item = lstViewDirsFiles.SelectedItems[0];
+                object obj = item.Tag;
+                FileInfo fileInfo = (FileInfo)obj;
+                ProcessStartInfo psi = new ProcessStartInfo
+                {
+                    FileName = fileInfo.FullName.ToString(),
+                    UseShellExecute = true
+                };
+
+                Process.Start(psi);
+            }
+        }
+
+        private void tsmCompress_Click(object sender, EventArgs e)
+        {
+            if (lstViewDirsFiles.SelectedItems.Count > 0)
+            {
+                for(int i=0;i<lstViewDirsFiles.SelectedItems.Count;i++)
+                {
+                    ListViewItem item = lstViewDirsFiles.SelectedItems[i];
+                    object obj = item.Tag;
+                    FileInfo fileInfo = obj as FileInfo;
+                    if (fileInfo != null)
+                    {
+                        string fileName = fileInfo.FullName.ToString();                        
+                        Compress(fileName, "TESLAAA.zip");
+                    }
+                    if (obj != null)
+                    {
+                        DirectoryInfo di = obj as DirectoryInfo;
+                        if(di != null)  Compress(di.FullName, "TESTAAA.zip");
+                     
+                    }
+                }
+
+                PopulateFilesAndDirectories(directoryInfo);
+            }
+        }
+
+
+        static void Compress(string inputPath, string outputPath)
+        {
+            if (Directory.Exists(inputPath))
+            {
+                System.IO.Compression.ZipFile.CreateFromDirectory(inputPath, outputPath);
+            }
+
+            else if (File.Exists(inputPath))
+            {
+                if (File.Exists(outputPath)) // if zip already exists
+                {
+                    using (System.IO.Compression.ZipArchive archive = System.IO.Compression.ZipFile.Open(outputPath, ZipArchiveMode.Update))
+                    {
+                        archive.CreateEntryFromFile(inputPath, Path.GetFileName(inputPath),CompressionLevel.SmallestSize);
+                        return;
+                    }
+                }               
+
+                using (System.IO.Compression.ZipArchive archive = System.IO.Compression.ZipFile.Open(outputPath, ZipArchiveMode.Update))
+                {
+                    archive.CreateEntryFromFile(inputPath, Path.GetFileName(inputPath), CompressionLevel.SmallestSize);
+                    return;
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Input path does not exist.");
+            }
+        }
+
+        static void CompressFolder(string folderPath, ZipOutputStream zipStream, string parentFolder)
+        {
+            string[] files = Directory.GetFiles(folderPath);
+            foreach (string filePath in files)
+            {
+                string relativePath = Path.Combine(parentFolder, Path.GetFileName(filePath));
+                AddFileToZip(zipStream, filePath, relativePath);
+            }
+
+            string[] folders = Directory.GetDirectories(folderPath);
+            foreach (string folder in folders)
+            {
+                string folderName = Path.GetFileName(folder);
+                string relativePath = Path.Combine(parentFolder, folderName + "/");
+                CompressFolder(folder, zipStream, relativePath);
+            }
+        }
+
+        static void AddFileToZip(ZipOutputStream zipStream, string filePath, string relativePath)
+        {
+            ZipEntry entry = new ZipEntry(relativePath);
+            zipStream.PutNextEntry(entry);
+
+            using (FileStream fileStream = File.OpenRead(filePath))
+            {
+                fileStream.CopyTo(zipStream);
+            }
+
+            zipStream.CloseEntry();
+        }
+
+
+    }
+}
